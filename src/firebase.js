e8fe811c50bafeddef52988ab2904fbf73b45376
Pranav Dhiman <pranav4914.se24@chitkara.edu.in>
2025-10-18 16:58:46 +0530

    Fix routes and Netlify redirects

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";        
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // <-- You were missing this
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA_k0V7Y5oZq5Yow8PNAadKhshrvSob-aI",
   authDomain: "myproject2-c9a37.firebaseapp.com",
   projectId: "myproject2-c9a37",
-  storageBucket: "myproject2-c9a37.firebasestorage.app",
+  storageBucket: "myproject2-c9a37.appspot.com", // corrected .app -> .appspot.com
   messagingSenderId: "645402503797",
   appId: "1:645402503797:web:37a5d803bff8722807a63c"
 };
@@ -18,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Exports
 export const auth = getAuth(app);
 export const db = getFirestore(app);
